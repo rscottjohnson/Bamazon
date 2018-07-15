ac2aee1b17c79bc4032158b1d4080f0d195ff046
Scott Johnson <sjohnson.wvu@gmail.com>
2018-07-14 21:34:18 -0400

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,15 @@
+/* 1. Create a MySQL Database called `bamazon`. */
+
+/* 2. Then create a Table inside of that database called `products`.
+
+3. The products table should have each of the following columns:
+
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores) */
---
+++ seeds.sql
@@ -0,0 +1,1 @@
+/* 4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table). */
