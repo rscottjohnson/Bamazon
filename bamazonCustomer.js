9f384ed16b65e593f1c8a6bee8a91c3083348233
Scott Johnson <sjohnson.wvu@gmail.com>
2018-07-20 23:19:41 -0400

    Added console.table to bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,9 +4,13 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var ctable = require("console.table");
 
 var divider =
-  "\n============================================================\n";
+  "============================================================\n";
+
+var resArray = [];
+var tableArray = [];
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -30,16 +34,24 @@ connection.connect(function (err) {
 
 // First display all of the items available for sale. Include the ids, names, and prices of products for sale.
 function showInventory() {
+  // Empty the arrays that support console.table
+  resArray = [];
+  tableArray = [];
+
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
 
     console.log(divider);
 
-    // Log all results of the SELECT statement
+    // Build the table array
     for (var i = 0; i < res.length; i++) {
-      console.log(
-        "Item ID: " + res[i].item_id + " | " + "Product Name: " + res[i].product_name + " | " + "Price: $" + res[i].price);
+      resArray.push(res[i].item_id, res[i].product_name, res[i].price);
+      tableArray.push(resArray);
+      resArray = [];
     }
+
+    // Console log the table
+    console.table(["Item ID", "Product Name", "Price ($)"], tableArray);
     console.log(divider);
     customerMenu();
   });
@@ -49,6 +61,10 @@ function showInventory() {
 // * The ID of the product they would like to buy
 // * How many units of the product they would like to buy
 function customerMenu() {
+  // Empty the arrays that support console.table
+  resArray = [];
+  tableArray = [];
+
   connection.query("SELECT * FROM products", function (err, res) {
     inquirer
       .prompt([{
@@ -80,18 +96,25 @@ function customerMenu() {
         connection.query("SELECT * FROM products WHERE ?", {
           item_id: answer.prodPick
         }, function (err, res) {
+
+          // Build the table array
+          resArray.push(res[0].item_id, res[0].product_name, res[0].price);
+          tableArray.push(resArray);
+
+          // Console log the table
           console.log(divider);
-          console.log("Item ID: " + res[0].item_id + " | " + "Product Name: " + res[0].product_name + " | " + "Price: $" + res[0].price);
+          console.table(["Item ID", "Product Name", "Price ($)"], tableArray);
+          console.log(divider);
 
           // Check for enough product inventory
           if (res[0].stock_quantity < answer.prodUnits) {
-            // If not, log `Insufficient quantity!`, and stop the order
+            // If not, log `Insufficient quantity`, and stop the order
             console.log("Sorry, we have insufficient quantity to complete your order.");
             console.log(divider);
             connection.end();
           } else {
             // If inventory available, fulfill the order.
-            // * Update the SQL database to the remaining quantity
+            // Update the SQL database to the remaining quantity
             connection.query(
               "UPDATE products SET ? WHERE ?", [{
                   stock_quantity: (res[0].stock_quantity - answer.prodUnits)
@@ -101,9 +124,18 @@ function customerMenu() {
                 }
               ],
               function (error) {
+                // Empty the arrays that support console.table
+                resArray = [];
+                tableArray = [];
+
                 if (error) throw err;
-                // * Show the customer the total purchase cost
-                console.log("Order quantity: " + answer.prodUnits + " | " + "Price: $" + res[0].price + " | " + "Total purchase: $" + (answer.prodUnits * res[0].price));
+
+                // Build the table array
+                resArray.push(answer.prodUnits, res[0].price, (answer.prodUnits * res[0].price));
+                tableArray.push(resArray);
+
+                // Show the customer the total purchase cost by console logging the table
+                console.table(["Order quantity", "Price ($)", "Total Purchase ($)"], tableArray);
                 console.log(divider);
                 connection.end();
               }
